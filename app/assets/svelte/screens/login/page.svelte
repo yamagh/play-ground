50a8550ba568ebc862074c3d4853bbbcc608c935
yamagh <hiroyuki.yamagami@gmail.com>
2025-08-28 20:12:28 +0900

    ログイン画面：Sveltestrap のコンポーネントを使うように変更

--- app/assets/svelte/screens/login/page.svelte
+++ app/assets/svelte/screens/login/page.svelte
@@ -1,28 +1,29 @@
 <script lang="ts">
+  import { Button, Card, CardBody, FormGroup, Icon, Input } from "@sveltestrap/sveltestrap";
 </script>
 
 <div class="vh-100 d-flex justify-content-center align-items-center">
-  <div class="mx-auto card shadow p-5" style:width=25rem>
-    <div class="mb-5 text-center">
-      <i class="bi bi-person-fill-lock text-gray-300" style:font-size=10rem></i>
-    </div>
-    <div class="form-floating mb-3">
-      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-      <label for="floatingInput">Email address</label>
-    </div>
-    <div class="form-floating">
-      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-      <label for="floatingPassword">Password</label>
-    </div>
-    <div class="mt-5 d-grid">
-      <button type="submit" class="btn btn-primary">Login</button>
-    </div>
+  <div class="mx-auto" style:width=25rem>
+    <Card class="shadow p-3">
+      <CardBody>
+        <div class="mb-5 text-center" style:font-size=10rem>
+          <Icon name="person-fill-lock" class=text-gray-300/>
+        </div>
+        <FormGroup floating label="Email">
+          <Input type="email" placeholder="name@example.com" />
+        </FormGroup>
+        <FormGroup floating label="Password" spacing=pb-4>
+          <Input type="password" placeholder="Password" />
+        </FormGroup>
+        <Button color=primary block>Login</Button>
+      </CardBody>
+    </Card>
   </div>
 </div>
 
 
 <style>
   :global(body) {
-    background-color: var(--bs-tertiary-color);
+    background-color: var(--bs-primary-bg-subtle);
   }
 </style>
