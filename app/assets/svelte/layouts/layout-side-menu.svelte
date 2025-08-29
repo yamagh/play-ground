eb0f993ede2994b33fa30a71a2927c82db9169f3
yamagh <hiroyuki.yamagami@gmail.com>
2025-08-29 21:10:22 +0900

    feat: サイドメニューの開閉機能を追加

--- app/assets/svelte/layouts/layout-side-menu.svelte
+++ app/assets/svelte/layouts/layout-side-menu.svelte
@@ -6,10 +6,12 @@
     DropdownMenu,
     DropdownToggle,
   } from '@sveltestrap/sveltestrap';
+  import{ slide } from 'svelte/transition'
 
   let {
     activeMenu = "Home",
     collapsibleMenu = true,
+    sidebarOpened = true,
     children = ()=>{}
   } = $props()
 
@@ -38,16 +40,16 @@
       label: "Customers",
       icon: "person-circle",
       href: "#",
-    }
+    },
   ]
 </script>
 
-<div class="vw-100 shadow-sm border-bottom p-2 d-flex justify-content-between sticky-top bg-white align-items-center fs-5">
+<div class="vw-100 shadow-sm border-bottom p-2 d-flex justify-content-between sticky-top bg-white align-items-center fs-5 position-fixed">
   <div>
     {#if collapsibleMenu}
-      <span class="ms-4">
-        <Icon name="layout-sidebar" />
-        <div class="vr ms-2"></div>
+      <span class="ms-3">
+        <Icon name="layout-sidebar" onclick={()=>{sidebarOpened = !sidebarOpened}} role="button"/>
+        <div class="vr ms-3"></div>
       </span>
     {/if}
     <Icon name="rocket-fill" class="mx-2"/>
@@ -72,24 +74,55 @@
     </Dropdown>
   </div>
 </div>
-<div class="vh-100 d-flex">
-  <div class="h-100 d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
-    <ul class="nav nav-pills flex-column mb-auto">
+<div class="vh-100 d-flex pt-5">
+  <div class="d-flex flex-column flex-shrink-0 bg-light border-end sidebar {sidebarOpened ? "sidebar-opened" : "sidebar-closed"} {sidebarOpened ? "p-3" : ''}">
+    <ul class="nav nav-pills flex-column mb-auto {sidebarOpened ? "" : "nav-flush text-center"}">
       {#each menuItemList as item }
         <li class="nav-item">
-          <a href="{item.href}" class="nav-link {item.label == activeMenu ? "active" : "link-dark"}" aria-current="page">
-            <Icon name={item.icon} class="me-2" />
-            {item.label}
+          <a
+            href="{item.href}"
+            class="nav-link {item.label == activeMenu ? "active" : "link-dark"} {sidebarOpened ? "" : "py-3 border-bottom"}"
+            data-bs-toggle={sidebarOpened ? "" : "tooltip"}
+            data-bs-placement={sidebarOpened ? "" : "right"}
+            data-bs-title={sidebarOpened ? "" : item.label}
+            aria-current="page"
+          >
+            <Icon name={item.icon}/>
+            {#if sidebarOpened}
+              <span class="ms-2">{item.label}</span>
+            {/if}
           </a>
         </li>
       {/each}
     </ul>
   </div>
-  <div class="border-start flex-grow-1 p-3">
+  <div class="flex-grow-1 p-3 main-content">
     {@render children()}
   </div>
 </div>
 
-
 <style>
+.sidebar {
+  height: calc(100vh - 47px);
+  overflow: auto;
+}
+.sidebar-opened {
+  width: 280px;
+  transition: 0.1s ease;
+}
+.sidebar-closed {
+  width: 64px;
+  transition: 0.1s ease;
+  a {
+    font-size: 1.4rem;
+  }
+}
+.nav-flush .nav-link {
+  border-radius: 0;
+}.nav-flush .nav-link {
+  border-radius: 0;
+}
+.main-content {
+  overflow: auto;
+}
 </style>
